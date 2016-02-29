f1ccf67a1499716995114f75ef183e13b581e711
jaykoski <koskijes@gmail.com>
2016-02-29 17:08:36 +0200

    Cleaned some code

--- Sensor_read.ino
+++ Sensor_read.ino
@@ -1,11 +1,10 @@
 #include <Sensirion.h>
 
-#define ENA_ERRCHK  // Enable error checking code
 
 const byte dataPin =  2;                 // SHTxx serial data
 const byte sclkPin =  3;                 // SHTxx serial clock
 const byte ledPin  = 13;                 // Arduino built-in LED
-const unsigned long TRHSTEP   = 60000UL;  // Sensor query period, Getting readins every 1min.
+const unsigned long TRHSTEP   = 50000UL;  // Sensor query period
 const unsigned long BLINKSTEP =  250UL;  // LED blink period
 
 Sensirion sht = Sensirion(dataPin, sclkPin);
@@ -22,32 +21,15 @@ byte measType = TEMP;
 unsigned long trhMillis = 0;             // Time interval tracking
 unsigned long blinkMillis = 0;
 
-
-// This version of the code checks return codes for errors
-byte error = 0;
-
 void setup() {
-  byte stat;
   Serial.begin(9600);
   pinMode(ledPin, OUTPUT);
   delay(15);                             // Wait >= 11 ms before first cmd
-  // Demonstrate status register read/write
-  if (error = sht.readSR(&stat))         // Read sensor status register
-    logError(error);
-  Serial.print("Status reg = 0x");
-  Serial.println(stat, HEX);
-  if (error = sht.writeSR(LOW_RES))      // Set sensor to low resolution
-    logError(error);
-  if (error = sht.readSR(&stat))         // Read sensor status register again
-    logError(error);
-  Serial.print("Status reg = 0x");
-  Serial.println(stat, HEX);
+  
   // Demonstrate blocking calls
-  if (error = sht.measTemp(&rawData))    // sht.meas(TEMP, &rawData, BLOCK)
-    logError(error);
+  sht.measTemp(&rawData);    // sht.meas(TEMP, &rawData, BLOCK)
   temperature = sht.calcTemp(rawData);
-  if (error = sht.measHumi(&rawData))    // sht.meas(HUMI, &rawData, BLOCK)
-    logError(error);
+  sht.measHumi(&rawData);    // sht.meas(HUMI, &rawData, BLOCK)
   humidity = sht.calcHumi(rawData, temperature);
   dewpoint = sht.calcDewpoint(humidity, temperature);
   logData();
@@ -68,18 +50,14 @@ void loop() {
   if (curMillis - trhMillis >= TRHSTEP) {      // Time for new measurements?
     measActive = true;
     measType = TEMP;
-    if (error = sht.meas(TEMP, &rawData, NONBLOCK)) // Start temp measurement
-      logError(error);
+    sht.meas(TEMP, &rawData, NONBLOCK); // Start temp measurement
     trhMillis = curMillis;
   }
-  if (measActive && (error = sht.measRdy())) { // Check measurement status
-    if (error != S_Meas_Rdy)
-      logError(error);
+  if (measActive && sht.measRdy()) { // Check measurement status
     if (measType == TEMP) {                    // Process temp or humi?
       measType = HUMI;
       temperature = sht.calcTemp(rawData);     // Convert raw sensor data
-      if (error = sht.meas(HUMI, &rawData, NONBLOCK)) // Start humi measurement
-        logError(error);
+      sht.meas(HUMI, &rawData, NONBLOCK); // Start humi measurement
     } else {
       measActive = false;
       humidity = sht.calcHumi(rawData, temperature); // Convert raw sensor data
@@ -98,20 +76,3 @@ void logData() {
   Serial.print(dewpoint); 
 }
 
-// The following code is only used with error checking enabled
-void logError(byte error) {
-  switch (error) {
-    case S_Err_NoACK:
-      Serial.println("Error: No response (ACK) received from sensor!");
-      break;
-    case S_Err_CRC:
-      Serial.println("Error: CRC mismatch!");
-      break;
-    case S_Err_TO:
-      Serial.println("Error: Measurement timeout!");
-      break;
-    default:
-      Serial.println("Unknown error received!");
-      break;
-  }
-}
